0464f8e74f7534a5f848b8507baa6545b58c8741
roronya <roronya628@gmail.com>
2022-12-04 16:08:36 +0900

    add lexer test

--- y_test.go
+++ y_test.go
@@ -1,18 +1,40 @@
 package main
 
 import (
-	"log"
 	"strings"
 	"testing"
-	"text/scanner"
 )
 
-func TestLexerSplit(t *testing.T) {
-	l := newLexer()
-	l.Init(strings.NewReader("アイカツ！"))
-	l.Mode = scanner.ScanChars
-	for v := string(l.Scan()); v != "！"; v = string(l.Scan()) {
-		log.Printf(v)
-		log.Printf("%#v", v == "！")
+// Lexerがトークンを認識しているか、想定通りの内部形式に変換しているか
+func TestLexer_Lex(t *testing.T) {
+	s := "くるくるきゃわわ♡オケオケオッケー！アイカツ！らぶゆ〜♡それアイカツか？世界の中心はここね♡血を吸うわよ！穏やかじゃない！"
+	r := strings.NewReader(s)
+	l := newLexer(r)
+	lval := new(yySymType)
+
+	wants := []int{int('>'), int('<'), int('+'), int('-'), int('['), int(']'), int(','), int('.')}
+	for _, want := range wants {
+		got := l.Lex(lval)
+		if want != got {
+			t.Errorf("want %d, but got %d", want, got)
+		}
 	}
+}
+
+// 想定外の文字列に対してエラーを起こせるか
+func TestLexer_Error(t *testing.T) {
+	s := "hogehoge！"
+	r := strings.NewReader(s)
+	l := newLexer(r)
+	lval := new(yySymType)
+
+	defer func() {
+		err := recover()
+		if err != "syntax error" {
+			t.Errorf("want syntax error, but got %s", err)
+		}
+
+	}()
+
+	l.Lex(lval)
 }
